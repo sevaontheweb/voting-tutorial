b388ac3377d9673941544c3038bbb9183f8ea916
sevka <kolokolov-1986@yandex.ru>
2017-09-14 21:55:37 +0300

    Added server reactions on client actions

--- voting-server/index.js
+++ voting-server/index.js
@@ -4,3 +4,9 @@ import {startServer as StartServer} from './src/server';
 
 export const store = makeStore();
 StartServer(store);
+
+store.dispatch({
+    type: 'SET_ENTRIES',
+    entries: require('./entries.json')
+});
+store.dispatch({type: 'NEXT'});
--- voting-server/src/server.js
+++ voting-server/src/server.js
@@ -5,5 +5,10 @@ export default function startServer(store) {
 
     store.subscribe(
         () => io.emit('state', store.getState().toJS())
-    )
+    );
+
+    io.on('connection', (socket) => {
+        socket.emit('state', store.getState().toJS());
+        socket.on('action', store.dispatch.bind(store));
+    });
 }
