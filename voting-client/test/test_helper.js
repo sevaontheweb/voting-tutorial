58037a46b1ad39d075a80532570096041ea70569
sevka <kolokolov-1986@yandex.ru>
2017-09-16 21:21:22 +0300

    Jsdom test fixed

--- voting-client/test/test_helper.js
+++ voting-client/test/test_helper.js
@@ -4,16 +4,14 @@ import chaiImmutable from 'chai-immutable';
 
 const { JSDOM } = jsdom;
 
-const doc = new JSDOM('<!doctype html><html><body></body></html>');
-//const win = doc.defaultView;
-const win = doc.window;
+const dom = new JSDOM('<!doctype html><html><body></body></html>');
 
-global.document = doc;
-global.window = win;
+global.window = dom.window;
+global.document = dom.window.document;
 
-Object.keys(window).forEach((key) => {
-  if (!(key in global)) {
-    global[key] = window[key];
+Object.keys(dom.window).forEach((property) => {
+  if (!(property in global)) {
+    global[property] = window[property];
   }
 });
 
