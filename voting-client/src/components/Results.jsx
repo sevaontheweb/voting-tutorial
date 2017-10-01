cd3691efb81418b4ce09ba53c3e3d3ef898c81ff
sevka <kolokolov-1986@yandex.ru>
2017-10-01 22:17:17 +0300

    Results component changed to be pure JavaScript class instead of React.createClass

--- voting-client/src/components/Results.jsx
+++ voting-client/src/components/Results.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 
-export default React.createClass({
-  mixins: [PureRenderMixin],
-  getPair: function() {
+export default class Results extends React.Component {
+  constructor(props) {
+    super(props)
+    this.mixins = [PureRenderMixin];
+  }
+
+  getPair() {
     return this.props.pair || [];
-  },
-  render: function() {
+  };
+
+  getVotes(entry) {
+    if (this.props.tally && this.props.tally.has(entry)) {
+      return this.props.tally.get(entry);
+    }
+    return 0;
+  };
+
+  render() {
     return <div className="results">
       {this.getPair().map(entry =>
         <div key={entry} className="entry">
           <h1>{entry}</h1>
+          <div className="voteCount">
+            {this.getVotes(entry)}
+          </div>
         </div>
       )}
     </div>;
   }
-});
+};
